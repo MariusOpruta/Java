2aab5711d16175c40271983549ffbe1c206cae49
MariusOpruta <opmasorin@gmail.com>
2023-01-02 20:20:27 +0200

    tema7

--- Tema7/ProblemaArray.java
+++ Tema7/ProblemaArray.java
@@ -1,5 +1,16 @@
 public class ProblemaArray {
     public static void main(String[] args) {
-
+        String[] string1 = new String[5];
+            string[0] = "mar";
+            string[1] = "portocala";
+            string[2] = "caisa";
+            string[3] = "piersica";
+            string[4] = "ananas";
+        String[] string2 = new String[4];
+            string[0] = "dacia";
+            string[1] = "mercedes";
+            string[2] = "bmw";
+            string[3] = "kia";
+        System.out.println(string2.length);
     }
 }
