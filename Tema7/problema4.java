f27b5c2a41a68c03243d7c520823006c94b3c38d
MariusOpruta <opmasorin@gmail.com>
2023-01-03 20:12:02 +0200

    tema7 probl4(mai putin 4b count)

--- Tema7/problema4.java
+++ Tema7/problema4.java
@@ -1,14 +1,11 @@
 package curs7;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Collections;
+import java.util.*;
 
 import static java.lang.Integer.parseInt;
 
-public class problema4 {
-    static class ProblemaA{
+
+    class ProblemaArray{
         public static void main(String[] args) {
             List<Integer> rasturnat = new ArrayList<>(4);
             List<Integer> simpla = new ArrayList<>(4);
@@ -20,14 +17,33 @@ public class problema4 {
                 j = parseInt(scanner.nextLine());
                 simpla.add(j);
             }
-
             System.out.println("Array-ul este: " + simpla);
-            Collections.swap(simpla, 0, 3);
-            System.out.println("Rasturnat: " + simpla);
+            //Collections.swap(simpla, 0, 3);
+            // SE PUTEA FACE SI MAI SIMPLU CU Collection.swap si o singura lista dar nu era in enunt :)
+
+            int k;
+            for (k=3;k>=0;k--){
+                rasturnat.add(simpla.get(k));
+            }
+            System.out.println("Rasturnat: " + rasturnat);
+        }
+    }
+    class ProblemaListe {
+    public static void main(String[] args) {
+        List<Integer> lista = new ArrayList<>();
+        int i = 0;
+        while (i != 10) {
+            Random j = new Random();
+            lista.add(j.nextInt(30));
+            i++;
+            //System.out.println(lista);
         }
 
+        System.out.println(lista);
 
-
+        }
     }
 
-}
+
+
+
