c43110b1d5b627fcc12ffe9a3780bd0e5b7581c7
MariusOpruta <opmasorin@gmail.com>
2023-01-04 22:57:07 +0200

    tema7 probl4 rezolvare random(10)

--- Tema7/Tema7/problema4.java
+++ Tema7/Tema7/problema4.java
@@ -2,45 +2,44 @@ package Tema7;
 
 import java.util.*;
 
-import static java.lang.Integer.parseInt;
-
-
     class ProblemaArray{
         public static void main(String[] args) {
-            List<Integer> rasturnat = new ArrayList<>(4);
-            List<Integer> simpla = new ArrayList<>(4);
+            List<String> array1 = new ArrayList<>(4);
+            List<String> array2 = new ArrayList<>(4);
             Scanner scanner = new Scanner(System.in);
             int i;
-            int j;
+
             for (i = 0; i < 4; i++) {
                 System.out.println("Valoarea indexului " + i + " este: ");
-                j = parseInt(scanner.nextLine());
-                simpla.add(j);
+                String j = scanner.nextLine();
+                array1.add(j);
             }
-            System.out.println("Array-ul este: " + simpla);
+            System.out.println("Array-ul este: " + array1);
             //Collections.swap(simpla, 0, 3);
             // SE PUTEA FACE SI MAI SIMPLU CU Collection.swap si o singura lista dar nu era in enunt :)
 
             int k;
             for (k=3;k>=0;k--){
-                rasturnat.add(simpla.get(k));
+                array2.add(array1.get(k));
             }
-            System.out.println("Rasturnat: " + rasturnat);
+            System.out.println("Rasturnat: " + array2);
         }
     }
     class ProblemaListe {
     public static void main(String[] args) {
         List<Integer> lista = new ArrayList<>();
-        int i = 0;
-        while (i != 10) {
-            Random j = new Random();
-            lista.add(j.nextInt(30));
+        Random j = new Random();
+        int i= j.nextInt(30);
+        while (i>0 && i<30){
+            if(i==10) {
+                break;
+            }
+            lista.add(i);
             i++;
-            //System.out.println(lista);
+
+            System.out.println(lista);
         }
-
-        System.out.println(lista);
-
+        //System.out.println(lista);
         }
     }
 
