320c91997c8d06de4c4238b2febb46047781001a
MariusOpruta <opmasorin@gmail.com>
2023-02-06 21:42:38 +0200

    Probl 3

--- Tema10/probl3.java
+++ Tema10/probl3.java
@@ -3,20 +3,24 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class probl3 {
     public static void main(String[] args) throws IOException {
-        Path path = Paths.get("D:\\NJ\\Java\\Tema10\\cuvinte.txt");
+        Path path = Paths.get("E:\\NJ\\Java\\Tema10\\fisier.txt");
         BufferedReader reader = Files.newBufferedReader(path);
-        String linie = "0";
-        int i = Integer.parseInt(linie);
+        String linie ;
+
         while( (linie = reader.readLine()) != null ) {
-            if(!linie.isEmpty()) {
-                i++;
+            List<String> litere =new ArrayList<>();
+            if (linie.contains(linie.toLowerCase())) {
+                litere.add(linie);
             }
-            System.out.println("Linia "+i+" are cuvantul "+linie+ " si are lungimea "+linie.length());
+          System.out.println(litere);
         }
     }
+
 }
 
